c7eb1f913215e56222ccc7f8c89aed8c78e77b4a
bfranzen19 <btfranzen@gmail.com>
2019-01-22 14:38:58 -0700

    inital commit. req functional

---
+++ server.js
@@ -0,0 +1,40 @@
+
+const express = require('express')
+const app = express()
+const request = require('request')
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.get('/', function(req,res) {
+    request({
+        url:`https://public-api.wordpress.com/wpcom/v2/work-with-us`,
+        headers: {
+            'X-future':
+                'automattician'
+
+        }
+
+    }, function(err, res, bod) {
+        if(err || (res.statusCode !==200)) {
+            console.log('error --> ', err)
+            console.log('res.statusCode', res.statusCode)
+            res.send('uh oh... ')
+        } else {
+            var bodyAsOb = JSON.parse(bod)
+            console.log('   -->    ', bod)
+            //res.send({results: bodyAsOb})
+        }
+    })
+})
+
+
+
+app.listen(8080, function() {
+    console.log('app listening on 8080')
+})
+
+
+// navigate to localhost:8080 to recieve the secret from the GET req
